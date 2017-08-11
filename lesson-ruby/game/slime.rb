49326b5f34a9375ad04b5eca82b0e1f3832ffb05
kari <kari@gmail.com>
2017-08-11 08:06:54 +0000

    init

---
+++ lesson-ruby/game/slime.rb
@@ -0,0 +1,26 @@
+require './character'
+
+module TechAcademy
+  module RPG
+    module Characters
+      class Slime < Character
+        @@type = 'スライム'
+        
+        attr_accessor :suffix
+        
+        def initialize(suffix)
+          super(10, 3)
+          self.suffix = suffix
+        end
+        
+        def name
+          @@type + self.suffix
+        end
+        
+        def self.description
+          puts @@type + 'は、最弱のモンスターだ！'
+        end
+      end
+    end
+  end
+end
