c6ccb5f9aee52630adae4ef9b51ad762c08b56da
kari <kari@gmail.com>
2017-09-04 22:35:15 +0000

    user login

--- microposts/db/schema.rb
+++ microposts/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904210451) do
+ActiveRecord::Schema.define(version: 20170904220817) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
