39a078c9373ebd06a9f5fbdd6f2676394a31e5ce
kari <kari@gmail.com>
2017-09-04 11:47:45 +0000

    init

--- microposts/app/models/micropost.rb
+++
@@ -1,8 +0,0 @@
-class Micropost < ApplicationRecord
-  belongs_to :user
-  has_many :favorites
-  has_many :users, through: :favorites, source: :user
-  
-  validates :user_id, presence: true
-  validates :favorite_id, presence: true, length: { maximum: 255 }
-end
